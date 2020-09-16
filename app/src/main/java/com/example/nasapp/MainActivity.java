d68fcef09d552fbd4d5a70d18a1f5ba17e3c94f4
ELMKV <ilya.makoveev021199@gmail.com>
2020-09-16 13:03:57 +0500

    Тестирование ретрофита и jsonplaceholder

--- app/src/main/java/com/example/nasapp/MainActivity.java
+++ app/src/main/java/com/example/nasapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.nasapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         textView = (TextView)findViewById(R.id.textView);
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPostWithID(1)
+                .enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
+                        Post post = response.body();
+
+                        textView.append(post.getId() + "\n");
+                        textView.append(post.getUserId() + "\n");
+                        textView.append(post.getTitle() + "\n");
+                        textView.append(post.getBody() + "\n");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
+
+                        textView.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
+
     }
+
+
+
 }
